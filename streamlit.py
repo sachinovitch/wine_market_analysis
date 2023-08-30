b79dca7965db7c0901b837daa58bff01f414302b
Bo Cao <bo_cao@yahoo.com>
2023-08-30 07:42:20 +0200

    streamlit ongoing

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+#functions to be defined:
+def print_recommendation(text):
+    pass
+
 #Here below is the code to define the side bar
 
 st.sidebar.text('\n\n\n')
@@ -8,11 +12,4 @@ st.sidebar.write("No, no wine over 150 euro.")
 st.sidebar.slider('Price range',0.0, 150.0, (10.0, 50.0))
 
 #Here below is the code to define the main page
-st.markdown("##Your recommondation will be below once you submit")
-# age = st.slider('How old are you?', 0, 130, 25)
-# st.write("I'm ", age, 'years old')
-
-# values = st.slider(
-#     'Select a range of values',
-#     0.0, 100.0, (25.0, 75.0))
-# st.write('Values:', values)
+st.title("Find the wine you like!")
