d58671cb88fa06197595000e1fc450f7abb8c0df
Bo Cao <bo_cao@yahoo.com>
2023-09-01 12:05:44 +0200

    added first page to present the query result

--- queries/country_with_budgets.sql
+++ queries/country_with_budgets.sql
@@ -1,10 +1,10 @@
-SELECT countries.name,
-        AVG(vintages.price_euros/vintages.bottle_volume_ml) AS avg
+SELECT countries.name AS Country,
+        ROUND(AVG((vintages.price_euros/vintages.bottle_volume_ml)*750), 2) AS Average_price_per_bottle
 FROM vintages JOIN wines
 ON vintages.wine_id = wines.id
 JOIN regions ON wines.region_id = regions.id
 JOIN countries ON regions.country_code = countries.code
-WHERE vintages.ratings_average >= 4.6
+WHERE vintages.ratings_average >= 4.2
 GROUP BY countries.name
-ORDER BY avg
-LIMIT 10
+ORDER BY Average_price_per_bottle
+LIMIT 3
--- queries/highlights10_apporoach1.sql
+++ queries/highlights10_apporoach1.sql
@@ -1,8 +1,10 @@
 SELECT 
-    name
+    name AS Name
 FROM vintages 
-WHERE  ratings_average > 4.6
+WHERE  ratings_average > 4.4
     AND
-    price_euros < 200
+    ratings_count > 500
+    AND
+    price_euros < 1000
 ORDER BY ratings_count DESC
 LIMIT 10
