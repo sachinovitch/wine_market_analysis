1b5f47a33f8db5e40d6ac965ddbbd74db534d85c
Henrique Rauen <henrique_rauen@yahoo.com.br>
2023-09-01 12:31:52 +0200

    Change webapp Icon

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -3,6 +3,7 @@
 #Created by Henrique Rauen (rickgithub@hsj.email)
 import streamlit as st
 
+st.set_page_config(page_title='Winino', page_icon='🍷', layout='wide')
 st.title("Find the wine you like!")
 st.markdown("## Welcome to our wine helper! Please choose a page on the left")
 st.image("assets/logo.png", width = 600)
